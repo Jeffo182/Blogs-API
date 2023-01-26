3fb81b374a176aa4242cbff0c6c9842d951d7ea5
Jefferson Felix <jefferson.felix182@gmail.com>
2023-01-26 18:13:31 -0300

    feat: migration blog_posts

--- src/migrations/20230126210656-categories.js
+++ src/migrations/20230126210656-categories.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('categories', { 
+    return await queryInterface.createTable('categories', { 
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
