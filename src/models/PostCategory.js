f5beff835bb24fd1c16e1e50f04db950dc6108ab
Jefferson Felix <jefferson.felix182@gmail.com>
2023-01-30 17:04:48 -0300

    feat: model PostCategory

---
+++ src/models/PostCategory.js
@@ -0,0 +1,37 @@
+"use strict";
+
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define(
+    "PostCategory",
+    {
+      postId: { type: DataTypes.INTEGER, primaryKey: true, foreignKey: true },
+      categoryId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        foreignKey: true,
+      },
+    },
+    {
+      tableName: "posts_categories",
+      underscored: true,
+      timestamps: false,
+    }
+  );
+
+  PostCategory.associate = ({ Category, BlogPost }) => {
+    BlogPost.belongsToMany(Category, {
+      as: "categories",
+      through: PostCategory,
+      foreignKey: "post_id",
+      otherKey: "category_id",
+    });
+    Category.belongsToMany(BlogPost, {
+      as: "posts",
+      through: PostCategory,
+      foreignKey: "category_id",
+      otherKey: "post_id",
+    });
+  };
+
+  return PostCategory;
+};
